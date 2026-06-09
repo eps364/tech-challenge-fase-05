834e415a02c65e0d87ea441889a527924bf60dae
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2026-06-09 20:35:49 -0300

    refactor(registry): align exception handling with RFC 9457 problem details

--- registry-service/src/main/java/br/com/fiap/registry/infra/exception/DomainException.java
+++ registry-service/src/main/java/br/com/fiap/registry/infra/exception/DomainException.java
@@ -1,52 +1,61 @@
+/* Copyright (c) 2024 FIAP. All rights reserved. */
 package br.com.fiap.registry.infra.exception;
 
 public abstract class DomainException extends RuntimeException {
 
-    private String instance;
-    private transient Object extensionData;
+  private String instance;
+  private transient Object extensionData;
 
-    public DomainException(String message) {
-        super(message);
-    }
+  public DomainException(String message) {
+    super(message);
+  }
 
-    public DomainException(String message, String instance) {
-        super(message);
-        this.instance = instance;
-    }
+  public DomainException(String message, String instance) {
+    super(message);
+    this.instance = instance;
+  }
 
-    public DomainException(String message, Throwable cause) {
-        super(message, cause);
-    }
+  public DomainException(String message, Throwable cause) {
+    super(message, cause);
+  }
 
-    public DomainException(String message, String instance, Throwable cause) {
-        super(message, cause);
-        this.instance = instance;
-    }
+  public DomainException(String message, String instance, Throwable cause) {
+    super(message, cause);
+    this.instance = instance;
+  }
 
-    public DomainException(String message, String instance, Object extensionData) {
-        super(message);
-        this.instance = instance;
-        this.extensionData = extensionData;
-    }
+  public DomainException(String message, String instance, Object extensionData) {
+    super(message);
+    this.instance = instance;
+    this.extensionData = extensionData;
+  }
 
-    public String getInstance() {
-        return instance;
-    }
+  public String getInstance() {
+    return instance;
+  }
 
-    public void setInstance(String instance) {
-        this.instance = instance;
-    }
+  public void setInstance(String instance) {
+    this.instance = instance;
+  }
 
-    public Object getExtensionData() {
-        return extensionData;
-    }
+  public Object getExtensionData() {
+    return extensionData;
+  }
 
-    public void setExtensionData(Object extensionData) {
-        this.extensionData = extensionData;
-    }
+  public void setExtensionData(Object extensionData) {
+    this.extensionData = extensionData;
+  }
 
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + "{" + "message='" + getMessage() + '\'' + ", instance='" + instance + '\'' + '}';
-    }
+  @Override
+  public String toString() {
+    return getClass().getSimpleName()
+        + "{"
+        + "message='"
+        + getMessage()
+        + '\''
+        + ", instance='"
+        + instance
+        + '\''
+        + '}';
+  }
 }
--- registry-service/src/main/java/br/com/fiap/registry/infra/exception/ProblemDetailBuilder.java
+++ registry-service/src/main/java/br/com/fiap/registry/infra/exception/ProblemDetailBuilder.java
@@ -1,89 +1,88 @@
+/* Copyright (c) 2024 FIAP. All rights reserved. */
 package br.com.fiap.registry.infra.exception;
 
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Fluent builder for creating RFC 9457 ProblemDetail objects.
- */
+/** Fluent builder for creating RFC 9457 ProblemDetail objects. */
 public class ProblemDetailBuilder {
 
-    private final ProblemDetail problemDetail = new ProblemDetail();
+  private final ProblemDetail problemDetail = new ProblemDetail();
 
-    public ProblemDetailBuilder type(String type) {
-        problemDetail.setType(type);
-        return this;
-    }
+  public ProblemDetailBuilder type(String type) {
+    problemDetail.setType(type);
+    return this;
+  }
 
-    public ProblemDetailBuilder status(int status) {
-        problemDetail.setStatus(status);
-        return this;
-    }
+  public ProblemDetailBuilder status(int status) {
+    problemDetail.setStatus(status);
+    return this;
+  }
 
-    public ProblemDetailBuilder title(String title) {
-        problemDetail.setTitle(title);
-        return this;
-    }
+  public ProblemDetailBuilder title(String title) {
+    problemDetail.setTitle(title);
+    return this;
+  }
 
-    public ProblemDetailBuilder detail(String detail) {
-        problemDetail.setDetail(detail);
-        return this;
-    }
+  public ProblemDetailBuilder detail(String detail) {
+    problemDetail.setDetail(detail);
+    return this;
+  }
 
-    public ProblemDetailBuilder instance(String instance) {
-        problemDetail.setInstance(instance);
-        return this;
-    }
+  public ProblemDetailBuilder instance(String instance) {
+    problemDetail.setInstance(instance);
+    return this;
+  }
 
-    public ProblemDetailBuilder timestamp(Instant timestamp) {
-        problemDetail.setTimestamp(timestamp);
-        return this;
-    }
+  public ProblemDetailBuilder timestamp(Instant timestamp) {
+    problemDetail.setTimestamp(timestamp);
+    return this;
+  }
 
-    public ProblemDetailBuilder traceId(String traceId) {
-        problemDetail.setTraceId(traceId);
-        return this;
-    }
+  public ProblemDetailBuilder traceId(String traceId) {
+    problemDetail.setTraceId(traceId);
+    return this;
+  }
 
-    public ProblemDetailBuilder correlationId(String correlationId) {
-        problemDetail.setCorrelationId(correlationId);
-        return this;
-    }
+  public ProblemDetailBuilder correlationId(String correlationId) {
+    problemDetail.setCorrelationId(correlationId);
+    return this;
+  }
 
-    public ProblemDetailBuilder extension(String key, Object value) {
-        problemDetail.addExtension(key, value);
-        return this;
-    }
+  public ProblemDetailBuilder extension(String key, Object value) {
+    problemDetail.addExtension(key, value);
+    return this;
+  }
 
-    public ProblemDetailBuilder extensions(Map<String, Object> extensions) {
-        if (extensions != null) {
-            problemDetail.getExtensions().putAll(extensions);
-        }
-        return this;
+  public ProblemDetailBuilder extensions(Map<String, Object> extensions) {
+    if (extensions != null) {
+      problemDetail.getExtensions().putAll(extensions);
     }
+    return this;
+  }
 
-    public ProblemDetail build() {
-        if (problemDetail.getStatus() == null) {
-            throw new IllegalStateException("Problem status must be set");
-        }
-        if (problemDetail.getTimestamp() == null) {
-            problemDetail.setTimestamp(Instant.now());
-        }
-        return problemDetail;
+  public ProblemDetail build() {
+    if (problemDetail.getStatus() == null) {
+      throw new IllegalStateException("Problem status must be set");
     }
+    if (problemDetail.getTimestamp() == null) {
+      problemDetail.setTimestamp(Instant.now());
+    }
+    return problemDetail;
+  }
 
-    public static ProblemDetailBuilder from(ProblemDetail problem) {
-        ProblemDetailBuilder builder = new ProblemDetailBuilder();
-        builder.type(problem.getType());
-        builder.status(problem.getStatus());
-        builder.title(problem.getTitle());
-        builder.detail(problem.getDetail());
-        builder.instance(problem.getInstance());
-        builder.timestamp(problem.getTimestamp());
-        builder.traceId(problem.getTraceId());
-        builder.correlationId(problem.getCorrelationId());
-        builder.extensions(new HashMap<>(problem.getExtensions()));
-        return builder;
-    }
+  public static ProblemDetailBuilder from(ProblemDetail problem) {
+    ProblemDetailBuilder builder = new ProblemDetailBuilder();
+    builder.type(problem.getType());
+    builder.status(problem.getStatus());
+    builder.title(problem.getTitle());
+    builder.detail(problem.getDetail());
+    builder.instance(problem.getInstance());
+    builder.timestamp(problem.getTimestamp());
+    builder.traceId(problem.getTraceId());
+    builder.correlationId(problem.getCorrelationId());
+    builder.extensions(new HashMap<>(problem.getExtensions()));
+    return builder;
+  }
 }
