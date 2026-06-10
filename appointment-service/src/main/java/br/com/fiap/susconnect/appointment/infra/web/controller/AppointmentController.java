61de44a82b386eee553b3d38f906935c70a7ff3d
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2026-06-09 21:39:43 -0300

    refactor: clean up imports and enhance docker-compose for development

--- appointment-service/src/main/java/br/com/fiap/susconnect/appointment/infra/web/controller/AppointmentController.java
+++ appointment-service/src/main/java/br/com/fiap/susconnect/appointment/infra/web/controller/AppointmentController.java
@@ -1,6 +1,20 @@
 /* Copyright (c) 2024 FIAP. All rights reserved. */
 package br.com.fiap.susconnect.appointment.infra.web.controller;
 
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.com.fiap.susconnect.appointment.core.dto.AppointmentOutput;
 import br.com.fiap.susconnect.appointment.core.dto.ScheduleAppointmentRequest;
 import br.com.fiap.susconnect.appointment.core.usecase.CancelAppointmentUseCase;
@@ -11,19 +25,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.net.URI;
-import java.util.List;
-import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
--- auth-service/src/main/java/br/com/fiap/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/br/com/fiap/authservice/AuthServiceApplication.java
@@ -4,11 +4,9 @@ package br.com.fiap.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan(basePackages = "br.com.fiap.authservice")
 public class AuthServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(AuthServiceApplication.class, args);
